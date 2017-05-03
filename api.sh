ab287f056f345076916efc73153469d12cc59f27
Nicholas Guriev <guriev-ns@ya.ru>
2017-05-03 16:27:26 +0300

    Реализация методов API перенесена в отдельный файл, модуль документирован

---
+++ api.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+# Модуль для более простого написания ботов для Telegram. Определены две
+# публичные фукнции: tg::api_call и tg::start_bot. Для работы должна быть
+# установлена переменная окружения BOT_TOKEN.
+
+set -o errexit pipefail
+
+# Внутренние объекты для реализации постоянного соединения.
+_tg_socket_file="/var/tmp/$0.socket"
+coproc _tg_tls_service {
+  while true; do
+    openssl s_client -quiet -connect "api.telegram.org:443" \
+      -servername "api.telegram.org" 2>/dev/null
+  done
+}
+coproc _tg_socket_proxy {
+  while true; do
+    nc -lU "${_tg_socket_file}" <&${_tg_tls_service[0]} >&${_tg_tls_service[1]}
+  done
+}
+
+# Вызывает метод Telegram API, используя уже установленное постоянное
+# HTTPS-соединение с сервером.
+#
+# Параметры:
+#   $1   ~ имя метода, обязательный
+#   ост. ~ последовательность аргументов вида 'имя=значение'
+#
+# Результат в формате JSON печатается на стандартный вывод. В случае ошибки
+# возвращается её код. При непустой переменной NO_ERROR в поток ошибок не
+# выводятся диагностические сообщения.
+#
+# Пример:
+#   tg::api_call sendMessage text="Hello world!" chat_id=42 >/dev/null
+#
+function tg::api_call {
+  local url="http://api.telegram.org/bot${BOT_TOKEN}/$1"
+  declare -a params
+
+  for par in "${@:2}"; do
+    params+=("--data-urlencode" "${par}")
+  done
+
+  local data=$(curl --unix-socket "${_tg_socket_file}" --silent --show-error \
+    "${url}" "${params[@]}")
+
+  local status=$(echo "${data}" | jshon -e ok)
+  if [ "${status}" = "true" ]; then
+    echo "${data}" | jshon -e result
+  elif [ -z "${NO_ERROR}" ]; then
+    local code=$(echo "${data}" | jshon -e error_code)
+    local desc=$(echo "${data}" | jshon -e description -u)
+    echo "Error ${code}: ${desc}" >&2
+    return "${code}"
+  fi
+}
+
+# Запускает обработку входящих сообщений. Вызывает функции с названием вида
+# process_* в соответствии с принятым типом сообщения и заданными параметрами.
+# Каждое сообщение обрабатывается лишь единожды. Управление не возвращается.
+#
+# Параметры:
+#   $1 ~ Список обрабатываемых типов обновлений через запятую. Возможные типы:
+#        message, edited_message, channel_post, edited_channel_post,
+#        inline_query, chosen_inline_result, callback_query. При поступлении
+#        входящего обновления указанного типа вызывается функция process_тип.
+#        Необязательный, значение по умолчанию = messages.
+#   $2 ~ Список обрабатываемых команд бота через запятую. При поступлении
+#        указанной входящей команды вызывается функция process_название_command.
+#        Необязательный, приоритетнее $1.
+#   $3 ~ Список обрабатываемых типов сообщений через запятую. Возможные типы:
+#        text, audio, document, game, photo, sticker, video, voice, contact,
+#        location, venue, reply (пока не реализовано), service (не реализовано).
+#        При получении указанного сообщения вызывается функция process_тип.
+#
+function tg::start_bot {
+  local supported_updates_types=$(echo "${1:-messages}" \
+    | sed 's/^\s*/["/;s/\s*,\s*/","/g;s/\s*$/"]/')
+  local supported_commands_list=$(echo "$2" \
+    | sed 's/^\s*/\\(/;s/\s*,\s*/\\|/g;s/\s*$/\\)/')
+  local supported_message_types=$(echo "$3" \
+    | sed 's/^\s*/\\(/;s/\s*,\s*/\\|/g;s/\s*$/\\)/')
+  sleep 1
+
+  local last_update_id=0
+  while true; do
+    local updates=$(tg::api_call getUpdates offset="${last_update_id}" \
+      timeout=100 allowed_updates="${supported_updates_types}")
+    local incoming_updates=$(echo "${updates}" | jshon -l)
+    for (( i = 0; i < incoming_updates; i++ )); do
+      local current_update_id=$(echo "${updates}" | jshon -e $i -e update_id)
+      (( last_update_id = current_update_id + 1 ))
+
+      # Обработка команд
+      if echo "${updates}" \
+        | jshon -Q -e $i -e message -e text -u \
+        | grep -q "^/${supported_commands_list}"; then
+        local cmd=$(echo "${updates}" \
+          | jshon -e $i -e message -e text -u \
+          | sed 's,/'"${supported_commands_list}"',\1,')
+        echo "${updates}" | jshon -e $i -e message | "process_${cmd}_command"
+        continue
+      fi
+
+      # Обработка сообщений по типу
+      local content_type=$(echo "${updates}" \
+        | jshon -Q -e $i -e message -k \
+        | grep "${supported_message_types}")
+      if [ -n "${content_type}" ]; then
+        echo "${updates}" | jshon -e $i -e message | "process_${content_type}"
+        continue
+      fi
+
+      # Обработка прочих обновлений
+      local incoming=$(echo "${updates}" | jshon -e $i -k | grep -v update_id)
+      echo "${updates}" | jshon -e $i -e "${incoming}" | "process_${incoming}"
+    done
+  done
+}
--- bot.sh
+++ bot.sh
@@ -1,47 +1,13 @@
 #!/bin/bash
 
-# N.B.: должна быть установлена переменная окружения BOT_TOKEN
-
 set -o errexit pipefail
-
-socket_file="/var/tmp/$0.socket"
-coproc tls_service {
-  while true; do
-    openssl s_client -quiet -connect api.telegram.org:443 \
-      -servername api.telegram.org 2>/dev/null
-  done
-}
-coproc socket_proxy {
-  while true; do
-    nc -lU "${socket_file}" <&${tls_service[0]} >&${tls_service[1]}
-  done
-}
-
-function api_call {
-  local url="http://api.telegram.org/bot${BOT_TOKEN}/$1"
-  declare -a params
-
-  for par in "${@:2}"; do
-    params+=("--data-urlencode" "${par}")
-  done
-
-  local data=$(curl --unix-socket "${socket_file}" -sS "${url}" "${params[@]}")
-  local status=$(echo "${data}" | jshon -e ok)
-  if [ "${status}" = "true" ]; then
-    echo "${data}" | jshon -e result
-  elif [ -z "${NO_ERROR}" ]; then
-    local code=$(echo "${data}" | jshon -e error_code)
-    local desc=$(echo "${data}" | jshon -e description -u)
-    echo "Error ${code}: ${desc}" >&2
-    return "${code}"
-  fi
-}
+source api.sh
 
 function process_message {
   local message=$(cat)
   local msg_text=$(echo "${message}" | jshon -e text -u)
   local msg_chat=$(echo "${message}" | jshon -e chat -e id)
-  api_call sendMessage text="${msg_text}" chat_id="${msg_chat}" >/dev/null
+  tg::api_call sendMessage text="${msg_text}" chat_id="${msg_chat}" >/dev/null
 }
 
 function process_inline_query {
@@ -64,48 +30,4 @@ function process_sticker {
   cat
 }
 
-function start_bot {
-  local supported_updates_types=$(echo "${1:-messages}" \
-    | sed 's/^\s*/["/;s/\s*,\s*/","/g;s/\s*$/"]/')
-  local supported_commands_list=$(echo "$2" \
-    | sed 's/^\s*/\\(/;s/\s*,\s*/\\|/g;s/\s*$/\\)/')
-  local supported_message_types=$(echo "$3" \
-    | sed 's/^\s*/\\(/;s/\s*,\s*/\\|/g;s/\s*$/\\)/')
-  sleep 1
-
-  local last_update_id=0
-  while true; do
-    local updates=$(api_call getUpdates offset="${last_update_id}" timeout=100 \
-      allowed_updates="${supported_updates_types}")
-    local incoming_updates=$(echo "${updates}" | jshon -l)
-    for (( i = 0; i < incoming_updates; i++ )); do
-      local current_update_id=$(echo "${updates}" | jshon -e $i -e update_id)
-      (( last_update_id = current_update_id + 1 ))
-
-      # Обработка команд
-      if echo "${updates}" \
-        | jshon -Q -e $i -e message -e text -u \
-        | grep -q "^/${supported_commands_list}"; then
-        local cmd=$(echo "${updates}" \
-          | jshon -e $i -e message -e text -u \
-          | sed 's,/'"${supported_commands_list}"',\1,')
-        echo "${updates}" | jshon -e $i -e message | "process_${cmd}_command"
-        continue
-      fi
-
-      # Обработка сообщений по типу
-      local content_type=$(echo "${updates}" \
-        | jshon -Q -e $i -e message -k \
-        | grep "${supported_message_types}")
-      if [ -n "${content_type}" ]; then
-        echo "${updates}" | jshon -e $i -e message | "process_${content_type}"
-        continue
-      fi
-
-      # Обработка прочих обновлений
-      local incoming=$(echo "${updates}" | jshon -e $i -k | grep -v update_id)
-      echo "${updates}" | jshon -e $i -e "${incoming}" | "process_${incoming}"
-    done
-  done
-}
-start_bot "message,inline_query,chosen_inline_result" "start,list" "sticker"
+tg::start_bot "message,inline_query,chosen_inline_result" "start,list" "sticker"
